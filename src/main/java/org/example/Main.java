3527a16bdcd92fd9427b57cd6fd52790c4147024
Olga8I <olga.filonchik@gmail.com>
2024-06-17 21:55:36 +0300

    Third

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static int knapsack(int[] v, int[] w, int W)
+    {
+        int[][] T = new int[v.length + 1][W + 1];
+        for (int i = 1; i <= v.length; i++)
+        {
+            for (int j = 0; j <= W; j++)
+            {
+                if (w[i-1] > j) {
+                    T[i][j] = T[i-1][j];
+                }
+                else {
+                    T[i][j] = Integer.max(T[i-1][j], T[i-1][j-w[i-1]] + v[i-1]);
+                }
+            }
+        }
+        return T[v.length][W];
+    }
+    public static void main(String[] args)
+    {
+        int[] v = { 1, 2, 3, 8, 6, 4 };
+        int[] w = { 1, 2, 3, 8, 6, 4 };
+
+        int W = 25;
+
+        System.out.println(knapsack(v, w, W) == W);
     }
 }
